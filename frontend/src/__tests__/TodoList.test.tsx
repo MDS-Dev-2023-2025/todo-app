fb9dd17a2564a9a3f96edf0e781de45062db9f84
moussa <moussadeh92@gmail.com>
2025-06-05 09:37:31 +0200

    tests unitaires

--- frontend/src/__tests__/TodoList.test.tsx
+++ frontend/src/__tests__/TodoList.test.tsx
@@ -2,12 +2,15 @@ import { render, screen } from "@testing-library/react";
 import TodoList from "../components/TodoList";
 import RawTodoItem from "../models/RawTodoItem";
 
+// Deux tests unitaires pour le composant TodoList
+// 1. Vérifier que tous les todos passés en props sont affichés
+// 2. Vérifier que rien n'est affiché si la liste est vide
 describe("TodoList component test", () => {
   test("affiche tous les todos passés en props", () => {
     const exempleTodos: RawTodoItem[] = [
-      { id: "1", title: "title1", description: "desc1" },
-      { id: "2", title: "title2", description: "desc2" },
-      { id: "3", title: "title3", description: "desc3" },
+      { id: "1", title: "title1" },
+      { id: "2", title: "title2" },
+      { id: "3", title: "title3" },
     ];
 
     render(<TodoList todos={exempleTodos} />);
