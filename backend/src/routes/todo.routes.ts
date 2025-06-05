8491cc4915fa62cb544d7213c38c803dddb5bee8
Pierre Pineau <pierrepineau.pro@gmail.com>
2025-06-05 15:04:49 +0200

    C

--- backend/src/routes/todo.routes.ts
+++ backend/src/routes/todo.routes.ts
@@ -7,7 +7,6 @@ const todoController = new TodoController();
 router.get('/', todoController.getAllTodos);
 router.post('/', todoController.createTodo);
 router.put('/:id', todoController.updateTodo);
-router.puriehdt('/:id', todoController.updateTodo);
 router.delete('/:id', todoController.deleteTodo);
 
 export default router; 
